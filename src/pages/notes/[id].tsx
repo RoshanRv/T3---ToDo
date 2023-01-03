9fa484f78adb94590c5eff85d2cc75c32379a08d
Roshan <nkroshankumar@gmail.com>
2023-01-03 18:51:54 +0530

    Note Page and procedure to fetch Note is created

--- src/pages/notes/[id].tsx
+++ src/pages/notes/[id].tsx
@@ -1,9 +1,39 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
 import React from "react";
+import { number, string } from "zod";
+import Spinner from "../../components/Spinner";
+import { trpc } from "../../utils/trpc";
 
 const Note = () => {
+  const { id } = useRouter().query;
+  const { data: note, isLoading } = trpc.notes.getNoteById.useQuery({
+    id: Number(id),
+  });
+
   return (
-    <main className="flex min-h-screen w-full flex-col  bg-gradient-to-b from-[#2e026d] to-[#15162c]">
-      Note
+    <main className="flex min-h-screen w-full flex-col  gap-y-6 bg-gradient-to-b from-[#2e026d] to-[#15162c] p-6">
+      <Link href={"/"}>
+        <h1 className="text-2xl text-white">Back</h1>
+      </Link>
+
+      {/*          */}
+
+      <h1 className="text-3xl font-semibold text-white">Note</h1>
+
+      {isLoading ? (
+        <Spinner />
+      ) : (
+        <div className="mx-auto w-11/12 border-8 border-blue-700 bg-white px-6 py-3 md:w-9/12 lg:w-7/12">
+          {/*     title     */}
+          <h1 className=" text-3xl font-semibold text-purple-700 ">
+            {note?.title}
+          </h1>
+
+          {/*   desc   */}
+          <h1 className="text-lg">{note?.description}</h1>
+        </div>
+      )}
     </main>
   );
 };
--- src/server/trpc/router/notes.ts
+++ src/server/trpc/router/notes.ts
@@ -2,6 +2,16 @@ import { z } from "zod";
 import { publicProcedure, router } from "../trpc";
 
 export const notesRouter = router({
+  getNoteById: publicProcedure
+    .input(z.object({ id: z.number() }))
+    .query(async ({ ctx, input: { id } }) => {
+      return await ctx.prisma.notes.findUnique({
+        where: {
+          id,
+        },
+      });
+    }),
+
   getAllNotes: publicProcedure.query(async ({ ctx }) => {
     return await ctx.prisma.notes.findMany({
       orderBy: {
